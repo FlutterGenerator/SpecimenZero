b63f9e59723b15853fb767deb40daade7a33e798
FlutterGenerator <elementman50@yandex.com>
2025-12-21 20:15:15 +0500

    SpecimenZero

---
+++ app/src/main/java/com/android/support/Main.java
@@ -0,0 +1,32 @@
+package com.android.support;
+
+import android.app.Activity;
+import android.content.Context;
+
+public class Main {
+
+  static {
+    System.loadLibrary("DarkTeam");
+  }
+
+  private static native void CheckOverlayPermission(Context context);
+
+  public static void StartWithoutPermission(Context context) {
+    CrashHandler.init(context, true);
+    if (context instanceof Activity) {
+      // Check if context is an Activity.
+      Menu menu = new Menu(context);
+      menu.SetWindowManagerActivity();
+      menu.ShowMenu();
+    } else {
+      // Anything else, ask for permission
+      CheckOverlayPermission(context);
+    }
+  }
+
+  public static void Start(Context context) {
+    CrashHandler.init(context, false);
+
+    CheckOverlayPermission(context);
+  }
+}
---
+++ app/src/main/jni/Application.mk
@@ -0,0 +1,6 @@
+# To AIDE Users: If you are using 32-bit/ARMv7 phone, please remove arm64-v8a
+APP_ABI := armeabi-v7a
+APP_STL := c++_static
+APP_OPTIM := release
+APP_THIN_ARCHIVE := true
+APP_PIE := true
